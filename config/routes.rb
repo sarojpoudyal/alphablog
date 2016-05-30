4dff6bba767f1246f243799a6e3507477c3d1491
Saroj Sharma <saroj.webdeveloper@gmail.com>
2016-05-30 09:13:43 +0530

    lol

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     resources :comments
   end
   resources :tags
-  resources :authors
+  # resources :authors
   resources :author_sessions, only: [:new, :create, :destroy]
   get 'login', to: 'author_sessions#new'
   get 'logout', to: 'author_sessions#destroy'
